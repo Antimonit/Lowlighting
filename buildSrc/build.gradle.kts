05a0192488cc91a1999f27f0a486a7dd254c0641
David Khol <david@khol.me>
2022-05-14 01:58:00 +0900

    Move plugin versions back to the root build script

--- buildSrc/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-repositories {
-    gradlePluginPortal()
-    mavenCentral()
-}
-
-dependencies {
-    implementation("org.jetbrains.intellij.plugins:gradle-intellij-plugin:1.5.3")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
-}
