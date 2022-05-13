ba75b0cd2c5404d5128d36443f12b1b114591767
David Khol <david@khol.me>
2022-05-13 22:57:04 +0900

    Migrate build script to kts

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,9 @@
+repositories {
+    gradlePluginPortal()
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.intellij.plugins:gradle-intellij-plugin:1.5.3")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
+}
