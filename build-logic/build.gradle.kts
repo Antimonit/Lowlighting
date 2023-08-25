14a1c5cc53a094e97b7b24d59417ae09a71f043b
David Khol <david@khol.me>
2023-08-25 21:45:19 +0900

    Update the minimum intellij version to 2023.1

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.intellij.plugins:gradle-intellij-plugin:1.15.0")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0")
 
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
